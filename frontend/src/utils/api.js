72f7161367dbdd1b30a6dcf19ec3ca4c5ce67208
xyzcoaching <amansahu8969745790@gmail.com>
2026-04-11 01:18:39 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({ baseURL: 'https://aman-backend-ohpj.onrender.com' });
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
